05a1bcf9fc51f7d32671155cff704019e6f281d3
julien.demuynke <julien.demuynke.org>
2019-07-25 11:57:23 +0200

    The HRTF to be plotted can be chosen by the user + binauralization is optional

--- examples/plotListenHRTF.py
+++ examples/plotListenHRTF.py
@@ -1,3 +1,5 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
 # -*- coding: utf-8 -*-
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -7,7 +9,7 @@
 #   Example for getting HRTF data from a sofa file
 #   Plots the HRTF and convolves to obtain binaural sound
 #
-#   (C) Andrés Pérez-López - Eurecat / UPF
+#   (C) Andres Perez-Lopez - Eurecat / UPF
 #   30/08/2018
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -20,8 +22,8 @@ import numpy as np
 
 # Let's use subject_003 from the classical CIPIC database
 # http://sofacoustics.org/data/database/cipic/subject_003.sofa
-path = '/Volumes/Dinge/SOFA/subject_003.sofa'
-sofa = SOFAFile(path,'r')
+path = '/Users/acustica/Development_projects/matlabsofapanner/Deluxe_16384smp_Eurecat_nearfield_Koeln_anechoic.SOFA'
+sofa = SOFAFile(path, 'r')
 
 # File is actually not valid, but we can forgive them
 print "\n"
@@ -46,18 +48,20 @@ print "Variables"
 sofa.printSOFAVariables()
 
 # Let's check the position of the measurementa (Source position)
-sourcePositions = sofa.getVariableValue('SourcePosition')
-print "\n"
-print "Source Positions"
-print sourcePositions
-# and the info (units, coordinates)
-print sofa.getPositionVariableInfo('SourcePosition')
+sourcePosition = sofa.getVariableValue('SourcePosition')
+nof_source_positions = sofa.getDimension('M')
+SOFASourcePosition_Type = sofa.getPositionVariableInfo('SourcePosition')
+sourcePositions_spherical = SOFAConvertCoordinates(sourcePosition, SOFASourcePosition_Type[1], 'spherical', 'degree' if 'degree' in SOFASourcePosition_Type[0] else 'radian', 'degree')
+
+for i in range(nof_source_positions.size):
+    print(str(i+1) + ': ' + str(sourcePositions_spherical[i, :]))
 
 # Let's inspect the first measurement
-m = 0
+hrtf_index = input("Which HRIR do you want to plot?")
+m = int(hrtf_index)-1
 print "\n"
 print "Source Position of measurement " + str(m)
-print sourcePositions[m]
+print(str(sourcePositions_spherical[m, :]))
 # which is at 82 degrees azimuth, -7 degrees elevation
 
 # Read the data
@@ -77,18 +81,23 @@ plt.grid()
 plt.legend()
 plt.show()
 # It's pretty clear, based on the ITD and ILD, that the source is located at the left,
-# which on the other hand confirms the sourcePositions[0] information
+# which on the other hand confirms the sourcePosition[0] information
 
+binaural_process = raw_input("Do you want to binauralize a mono file with this HRTF? Y/N")
 
-# Let's render it with a file and listen to it
-# Open a mono wav file. I got this one from freesound
-# https://freesound.org/people/Ryntjie/sounds/365061/
-data, samplerate = sf.read('/Volumes/Dinge/audio/365061__ryntjie__pouring-cat-food-into-a-plastic-bowl.wav')
+if binaural_process == 'Y':
+    filename = raw_input("Please enter a filename:")
 
-# Convolve it with the hrtf
-binaural_left = scipy.signal.fftconvolve(data,hrtf[0])
-binaural_right = scipy.signal.fftconvolve(data,hrtf[1])
-binaural = np.asarray([binaural_left, binaural_right]).swapaxes(-1,0)
+    # Let's render it with a file and listen to it
+    # Open a mono wav file. I got this one from freesound
+    # https://freesound.org/people/Ryntjie/sounds/365061/
+    data, samplerate = sf.read(filename)
 
-# Write to a file, and enjoy!
-sf.write('/Volumes/Dinge/audio/binaural.wav', binaural, samplerate)
+    # Convolve it with the hrtf
+    binaural_left = scipy.signal.fftconvolve(data[:,0], hrtf[0])
+    binaural_right = scipy.signal.fftconvolve(data[:,1], hrtf[1])
+    binaural = np.asarray([binaural_left, binaural_right]).swapaxes(-1,0)
+
+    # Write to a file, and enjoy!
+    sf.write(filename[:-4] + '_binauralized.wav', binaural, samplerate)
+    print(filename[:-4] + '_binauralized.wav written!')
